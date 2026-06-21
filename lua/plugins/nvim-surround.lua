95dd65c491c33871f5023c1b206d8f16e41a58cb
Colin Doherty <colinjdoherty@outlook.com>
2026-06-20 17:25:41 -0700

    added nvim-surround

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,6 +1,6 @@
 return {
   "kylechui/nvim-surround",
-  version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+  -- version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
   -- Optional: See `:h nvim-surround.configuration` and `:h nvim-surround.setup` for details
   -- config = function()
